ee3cc9a59622e3b2a9da5a11a368f00a99a0b164
hemanth1-p <hemanthperabattula170@gmail.com>
2026-08-16 17:38:02 +0530

    Add DNS server configuration for MongoDB connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-
+const dns=require('dns');
 const connectDB = async () => {
   try {
+    dns.setServers(["8.8.8.8","1.1.1.1"]);
     const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bloodconnect';
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
